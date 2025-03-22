93164301eb2044900d2af4e03ccd54b7dc1d7082
Sagar Jogadia <sagarjogadia28@gmail.com>
2025-03-22 19:11:30 -0400

    - Add Day selector bar - Add parser for the date and add it to the UI

--- tracker/tracker_presentation/src/main/java/com/sagarjogadia28/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/sagarjogadia28/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -1,6 +1,9 @@
 package com.sagarjogadia28.tracker_presentation.tracker_overview
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
@@ -8,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.sagarjogadia28.core.util.UiEvent
 import com.sagarjogadia28.core_ui.LocalSpacing
+import com.sagarjogadia28.tracker_presentation.tracker_overview.components.DaySelector
 import com.sagarjogadia28.tracker_presentation.tracker_overview.components.NutrientsHeader
 import org.koin.androidx.compose.koinViewModel
 
@@ -27,6 +31,20 @@ fun TrackerOverviewScreen(
     ) {
         item {
             NutrientsHeader(state)
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            DaySelector(
+                date = state.date,
+                onPreviousDayClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnPrevDayClick)
+                },
+                onNextDayClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnNextDayClick)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = spacing.spaceMedium)
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
         }
     }
 }
